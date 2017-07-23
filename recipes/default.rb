362a9f9c33fcae71f6f32e2278b36100fa247a65
alnavabi <al.navabi@gmail.com>
2017-07-23 15:59:18 -0700

    Get the tomcat binaries

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,48 @@
-#
-# Cookbook:: tomcat
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# install open JDK 7 package
+
+package 'java-1.7.0-openjdk-devel'
+
+# add user and group for chef
+
+group 'chef' do
+
+  action :create
+
+end
+
+
+user 'chef' do
+
+  group 'chef'
+
+end
+
+
+# get and extract binaries
+
+
+remote_file '/tmp/apache-tomcat-8.0.45.tar.gz' do
+
+  source 'http://mirror.reverse.net/pub/apache/tomcat/tomcat-8/v8.0.45/bin/apache-tomcat-8.0.45.tar.gz'
+
+end
+
+
+
+directory '/opt/tomcat' do
+
+  action :create
+
+  recursive true
+
+end
+
+
+
+execute 'extract_tomcat' do
+
+  command 'tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
+
+  cwd '/tmp'
+
+end
